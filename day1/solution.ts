ca374ca03e87cd61c2ad2d31d40b98587bb30e36
yurist38 <yurist38@mail.ru>
2018-12-01 23:02:15 +0100

    day1: Add type definition

--- day1/solution.ts
+++ day1/solution.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 
 const pathToInput = path.join(__dirname, 'input.txt');
 const input = fs.readFileSync(pathToInput, 'utf8');
-const inputArray = input.split('\n').map(Number);
-const result = inputArray.reduce((acc, n) => (acc + n), 0);
+const inputArray: number[] = input.split('\n').map(Number);
+const result: number = inputArray.reduce((acc, n) => (acc + n), 0);
 
 console.log(result);
